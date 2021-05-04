ff3ccc2601064ef876f6a244067bf22900fdc648
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-05-04 11:04:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function createEmployeeRecord(firstName,familyName,title,payPerHour){
+  obj={
+    firstName,
+    familyName,
+    title,
+    payPerHour,
+    timeInEvents,
+    timeOutEvents
+  }
+  obj.firstName=firstName;
+  obj.familyName=familyName;
+  obj.title=title;
+  obj.payPerHour=payPerHour;
+  return obj
+}
